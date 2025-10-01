8f4218913912b887ba02ce44bb8570a20011486d
Rajagopal325 <rajagopalmbu@gmail.com>
2025-10-02 01:20:46 +0530

    Onload refresh

--- JS/app.js
+++ JS/app.js
@@ -21,6 +21,10 @@ function init() {
 
 init();
 
+//open chatbot automatically on refresh
+window.addEventListener("load", () => {
+  document.body.classList.add("show-chatbot");
+});
 
 function navbar(){
     const navbarContainer =document.querySelector(".nav-container");
